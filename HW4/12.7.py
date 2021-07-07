904d333969e7fb0ba60653a819322f9cd063f567
BrianCIS <85715367+BrianCIS@users.noreply.github.com>
2021-07-07 10:49:37 -0500

    Add files via upload

---
+++ HW4/12.7.py
@@ -0,0 +1,20 @@
+#Brian Rivera
+# 1922712
+
+def get_age():
+    age = int(input())
+    if(age>=18 and age<=75):
+        return age
+    else:
+        raise ValueError("Invalid age.")
+
+def fat_burning_heart_rate(age):
+    return ((220 - age)*(.7))
+
+if __name__ == '__main__':
+    try:
+        age = get_age()
+        print("Fat burning heart rate for a",age,"year-old:",fat_burning_heart_rate(age),"bpm")
+    except ValueError as ve:
+        print(ve.args[0])
+        print("Could not calculate heart rate info.\n")
